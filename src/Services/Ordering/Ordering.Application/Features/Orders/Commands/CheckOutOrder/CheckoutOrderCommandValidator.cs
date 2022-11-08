b6e122a2885e128b7d89e182a423291273d297bb
Jose-Luis Ballon <jlballonv@gmail.com>
2022-11-08 16:35:17 -0500

    Update Ordering.API

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/CheckOutOrder/CheckoutOrderCommandValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+
+namespace Ordering.Application.Features.Orders.Commands.CheckOutOrder
+{
+    public class CheckoutOrderCommandValidator : AbstractValidator<CheckoutOrderCommand>
+    {
+        public CheckoutOrderCommandValidator()
+        {
+            RuleFor(a => a.UserName)
+                .NotEmpty().WithMessage("{UserName} is required")
+                .NotNull()
+                .MaximumLength(50).WithMessage("{UserName} must not exceed 50 characters");
+
+            RuleFor(a => a.EmailAddress)
+                .NotEmpty().WithMessage("{EmailAddress} is required");
+
+            RuleFor(p => p.TotalPrice)
+                .NotEmpty().WithMessage("{TotalPrice} is required.")
+                .GreaterThan(0).WithMessage("{TotalPrice} should be greater than zero.");
+
+        }
+    }
+}
